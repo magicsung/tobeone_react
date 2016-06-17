3ee2b809dbaf297911ad99307cd2ccd75009df72
Jack Sung <magicsung@gmail.com>
2016-06-17 12:24:50 +0800

    Add email validate at register.

--- src/actions/userRegisterActions.js
+++ src/actions/userRegisterActions.js
@@ -2,9 +2,15 @@ import $ from 'jquery';
 import { browserHistory } from 'react-router';
 
 export function userRegister(loginInfo) {
+  function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
   return function(dispatch) {
     if (loginInfo.email == "") {
       dispatch(checkInfoFailure('Please enter email.'));
+    } else if (!validateEmail(loginInfo.email)) {
+      dispatch(checkInfoFailure('Please check email format.'));
     } else if (loginInfo.username == "") {
       dispatch(checkInfoFailure('Please enter your name.'));
     } else if (loginInfo.password == "") {
